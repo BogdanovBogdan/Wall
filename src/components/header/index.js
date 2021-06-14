c5ff4b5987d11c648183a7dedcdd5e0545344b82
Bogdan Bogdanov <bogdanov_bogdanv@mail.ru>
2021-06-14 09:34:02 +0300

    Init header scripts and styles

---
+++ src/components/header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './header.css';
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getLikeCount = this.getLikeCount.bind(this);
+  }
+
+  getLikeCount(data) {
+    return data.filter(item => item.liked).length;
+  }
+  
+  render() {
+    const { userName, data } = this.props;
+    console.log({ userName, data });
+    return (
+      <div className="app-header d-flex">
+        <h1>{userName}</h1>
+        <h2>{data.length} posts, {this.getLikeCount(data)} liked</h2>
+      </div>
+    )
+  }
+}
