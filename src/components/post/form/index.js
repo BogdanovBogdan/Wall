d0b73b57c01e23a45ae0e0d5111ec47428ea7c73
Bogdan Bogdanov <bogdanov_bogdanv@mail.ru>
2021-06-14 11:47:51 +0300

    Refactorin form script

--- src/components/post/form/index.js
+++ src/components/post/form/index.js
@@ -1,12 +1,34 @@
+import React from 'react';
 import './form.css';
 
-const Form = ({addItem}) => {
-  return (
-    <form className="bottom-panel d-flex" onSubmit={(event) => { event.preventDefault(); addItem('Privet'); }}>
-      <input type="text" placeholder="Type something" className="form-control new-post-label"/>
-      <button type="submit" className="btn btn-outline-secondary">Add</button>
-    </form>
-  )
-}
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-export default Form;
+  onChange(event) {
+    this.setState({ inputValue: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.addItem(this.state.inputValue); 
+    this.setState({ inputValue: '' });
+  }
+  
+  render() {
+    const { inputValue } = this.state;
+    
+    return (
+      <form className="bottom-panel d-flex" onSubmit={this.onSubmit}>
+        <input type="text" placeholder="Type something" className="form-control new-post-label" onChange={this.onChange} value={inputValue}/>
+        <button type="submit" className="btn btn-outline-secondary">Add</button>
+      </form>
+    )
+  }
+}
